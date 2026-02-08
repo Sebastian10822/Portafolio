d9e7188000be8107ebf86617a91fddefe3e72e8f
Sebastián Briñez <johansebastianbrinezparra@gmail.coml>
2026-02-08 14:54:13 -0500

    Update UI and styles

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -19,7 +19,7 @@ export const projectsData = [
   tech: ['React', 'Vite', 'TailwindCSS'],
   demo: 'https://sebastian10822.github.io/CYBERNEX-STORE/',
   github: 'https://github.com/Sebastian10822/CYBERNEX-STORE',
-  image: '/Imagenes/1.jpg'
+  image: 'Imagenes/cybernex.png'
 },
 
 {
