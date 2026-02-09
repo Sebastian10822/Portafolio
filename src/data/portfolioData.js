1c5a0398cee3c3a033befdb27387fabf3b019d3f
Sebastián Briñez <johansebastianbrinezparra@gmail.coml>
2026-02-08 22:40:08 -0500

    Update UI and styles

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,5 +1,5 @@
 import { Code, Palette, Database, Smartphone } from 'lucide-react';
-import CyberImg from '../../public/Imagenes/cybernex.png'
+
 
 
 
@@ -20,7 +20,7 @@ export const projectsData = [
   tech: ['React', 'Vite', 'TailwindCSS'],
   demo: 'https://sebastian10822.github.io/CYBERNEX-STORE/',
   github: 'https://github.com/Sebastian10822/CYBERNEX-STORE',
-  image: CyberImg
+  image: 'Imagenes/Cybernex.png'
 },
 
 {
